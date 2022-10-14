37b5883b3e797c91b2c401cf4a81108d32883159
Ricardo <Ricardoduinkerken@live.nl>
2022-10-14 13:20:27 +0200

    Add Dto's auth and create account

---
+++ AuthenticationService/AuthenticationService.Api/Dto/AuthenticationRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationService.Api.Dto;
+
+public class AuthenticationRequestDto
+{
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
---
+++ AuthenticationService/AuthenticationService.Api/Dto/CreateAccountRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationService.Api.Dto;
+
+public class CreateAccountRequestDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]
+    public string Password { get; set; }
+    [Required]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]
+    public string ConfirmPassword { get; set; }
+}
